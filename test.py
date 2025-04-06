3fc510261a0f192b39321bfa1664914c1e1db346
Pietro Cirigioni <pietrocirigioni@Mac.fritz.box>
2025-04-06 17:19:30 +0200

    casinologin

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-user_input = input("What's your name? ")
-print(f"Hello, {user_input}!")
+print("Welcome to Ciri's Casino!")
+
+name = input("Please enter your name: ")
+
+while len(name) < 2:
+    print("Please enter a valid name.\n")
+    name = input("Please re-enter your name: ")
+
+surname = input("Please enter your surname: ")
+while len(surname) < 2:
+    print("Please enter a valid surname.\n")
+    surname = input("Please re-enter your surname: ")   
+
+age = int(input("Please enter your age: "))
+while age < 18:
+    print("You must be 18 to play.\n")
+    age = int(input("Please re-enter your age: "))
+else:
+    print(f"Welcome {name} {surname}! \n")
+
+
+
